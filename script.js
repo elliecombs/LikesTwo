7d72f672d101d459e4d7a21506c334ebfab1de2d
ELLIE COMBS <ewxc96@gmail.com>
2022-01-28 21:14:27 -0800

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Ellie Box*/
+// var basket = 3
+// var element = document.querySelector("#count1")
+
+// function liked() {
+//     basket++
+//     element.innerText = basket + " Likes"
+//     console.log(basket)
+// }
+
+// /* Brian Box*/
+// var basket = 12
+// var element = document.querySelector("#count2")
+
+// function liked() {
+//     basket++
+//     element.innerText = basket + " Likes"
+//     console.log(basket)
+// }
+
+// /* Omar Box*/
+// var basket = 9
+// var element = document.querySelector("#count3")
+
+function liked(id) {
+    document.querySelector(id).innerHTML++ 
+}
